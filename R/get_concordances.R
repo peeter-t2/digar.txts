1bd5da964a41b1dab324544b1435064a7a162d45
Peeter Tinits <peeter.tinits@gmail.com>
2020-11-04 22:37:40 +0200

    fixes

--- R/get_concordances.R
+++ R/get_concordances.R
@@ -6,6 +6,7 @@
 #' @param subset Needs subset to be specified
 #' @keywords data
 #' @import data.table
+#' @import stringr
 #' @export
 #' @examples
 #' get_concordances("searchterm",texts,30,30,"txt","id")
@@ -16,8 +17,8 @@ get_concordances <- function(searchterm,texts,before=30,after=30,txtcolumn="txt"
   names(texts)[names(texts)==idcolumn] <- "id"
   texts <- data.table(texts)
   texts[,txt:=paste0(paste0(rep("-",before),collapse=""),txt,paste0(rep("-",after),collapse=""))]
-  found_index <- texts[, .(locs=unlist(str_locate_all(txt,searchterm))),by=id]#[order(locs),.SD,by=eval(idcolumn)]
-  found_index[,var:=rep(c("begin","end"),nrow(texts)/2)]
+  found_index <- texts[, .(locs=unlist(stringr::str_locate_all(txt,searchterm))),by=id][order(id,locs)]#[order(locs),.SD,by=eval(idcolumn)]
+  found_index[,var:=rep(c("begin","end"),nrow(found_index)/2)]
   found_index[,nr:=ceiling(seq_len(.N)/2),by=id]
   found_index_dcast <- dcast(found_index,id+nr~var,value.var = "locs")
   searching <- merge(texts,found_index_dcast,by="id")
