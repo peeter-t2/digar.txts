392faae45ff9cbc52ba3580bd76c04775a9d96a9
Peeter Tinits <peeter.tinits@gmail.com>
2020-11-12 06:31:09 +0200

    updated

--- R/get_subset_meta.R
+++ R/get_subset_meta.R
@@ -11,10 +11,15 @@
 
 get_subset_meta <- function(subset){
   subset <- data.table(subset)
+  mainpaper <- subset[,.N,keyid][order(-N)]
+  papers <- subset[,uniqueN(keyid)] -1
+  minyear <- subset[,(min(year))]
+  maxyear <- subset[,(max(year))]
+  nissues <- nrow(subset)
   metafiles <- subset[zippath_sections!="",unique(zippath_sections_meta)]
   collectionname <- "/gpfs/hpc/projects/digar_txt/text"
   metafilelist <- paste0(collectionname,"/meta_sections/", metafiles)
   subset_meta <- data.table::rbindlist(lapply(paste0("unzip -p ",metafilelist), function(x) data.table::fread(cmd=x,fill=T)),idcol=T)
-  system(paste0('printf "setupmeta \t $USER \t got corpus meta ', length(metafilelist) ,' files \t" >> /gpfs/hpc/projects/digar_txt/appendOnly_testDir/log1.txt; date +"%Y-%m-%d %T" >> /gpfs/hpc/projects/digar_txt/appendOnly_testDir/log1.txt'))
+  system(paste0('printf "setupmeta \t $USER \t got corpus meta ', length(metafilelist) ,' files:', nissues, " issues from", minyear, " to ", maxyear, " in ", mainpaper, " and ", papers, " other papers" ,'\t" >> /gpfs/hpc/projects/digar_txt/appendOnly_testDir/log1.txt; date +"%Y-%m-%d %T" >> /gpfs/hpc/projects/digar_txt/appendOnly_testDir/log1.txt'))
   return(subset_meta)
 }
